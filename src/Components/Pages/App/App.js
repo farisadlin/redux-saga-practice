40b3051b0b53f21382faeb42e59b05c6be51d592
farisadlin <farisnura@gmail.com>
2022-07-12 23:30:18 +0700

    Feat: Added some new cleaner structure of files

---
+++ src/Components/Pages/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import 'styles/App.css';
+import Navigation from 'Components/Pages/Navigation';
+import SinglePost from 'Components/Pages/SinglePost';
+import Home from 'Components/Pages/Home';
+
+function App () {
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<SinglePost />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
